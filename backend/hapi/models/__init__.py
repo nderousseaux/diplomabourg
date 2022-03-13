6167c3a9817f98e67380ee521813076b0c616bee
DIATTARA Amadou <diattara03@gmail.com>
2022-03-13 14:21:24 +0100

    amélioration databases

--- backend/hapi/models/__init__.py
+++ backend/hapi/models/__init__.py
@@ -1,3 +1,4 @@
+import imp
 from sqlalchemy.ext.declarative import declarative_base
 
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -15,11 +16,14 @@ from .mapModel import MapModel
 from .orderModel import OrderModel
 from .playerModel import PlayerModel
 from .powerModel import PowerModel
+from .typeRegionModel import TypeRegionModel
 from .regionModel import RegionModel
+from .regionETtypeRegion import regionEtTypeRegion
 from .stateModel import StateModel
 from .typeOrderModel import TypeOrderModel
 from .typeRegionModel import TypeRegionModel
 from .typeUnitModel import TypeUnitModel
+from .dipositionUnite import DispositionUniteModel
 from .unitModel import UnitModel
 
 #TODO: Changer tout les "" en ''
---
+++ backend/hapi/models/regionModel.py
@@ -0,0 +1,42 @@
+from sqlalchemy import *
+from sqlalchemy.orm import relationship
+
+from hapi.models import Base
+
+adjoining = Table(
+    'adjoining', Base.metadata,
+    Column('dst_region_id', Integer, ForeignKey('region.id')),
+    Column('src_region_id', Integer, ForeignKey('region.id')),
+    UniqueConstraint('dst_region_id', 'src_region_id', name='unique_adjoining'))
+
+class RegionModel(Base):
+    __tablename__= "region"
+
+    #Attributes
+    id = Column(Integer, primary_key=True )    
+    name=Column(String(45), nullable=False)
+    abreviation=Column(String(10), nullable=False)
+    power_id=Column(Integer, ForeignKey('power.id'))
+    hasCenter=Column(String(45))
+
+    
+    #Relationships
+    power = relationship('PowerModel', backref="regions")
+    type = relationship('TypeRegionModel',secondary='regionEtTypeRegion',  backref="regions")
+    dispositionUnite=relationship('dispositionUnite',backref="region")  
+
+    units_src_region = relationship('UnitModel', back_populates="src_region")
+    units_cur_region = relationship('UnitModel', back_populates="cur_region")
+    orders_src_region = relationship('OrderModel', back_populates="src_region")
+    orders_dst_region = relationship('OrderModel', back_populates="dst_region")
+
+
+    neighbours = relationship("RegionModel", secondary=adjoining, 
+                           primaryjoin=id==adjoining.c.src_region_id,
+                           secondaryjoin=id==adjoining.c.dst_region_id)
+
+
+    def beNeigbours(self, neighbour): #TODO: Dire à amadou
+        if neighbour not in self.neighbours:
+            self.neighbours.append(neighbours)
+            neighbour.neighbours.append(self)
