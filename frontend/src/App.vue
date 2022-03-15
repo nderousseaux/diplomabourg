8ce1b1fefa0ab6816ab102221b914c8ff1970e35
Loïc DIEBOLT <loic@dblt.fr>
2022-03-15 14:42:08 +0100

    Fixed indentation consistency

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,10 @@
 <template>
-  <Header />
-  <main>
-    <router-view></router-view>
-  </main>
-  <Chat />
-  <Footer />
+	<Header />
+	<main>
+		<router-view></router-view>
+	</main>
+	<Chat />
+	<Footer />
 </template>
 
 
@@ -14,42 +14,42 @@ import Footer from '@/components/Footer.vue'
 import Chat from '@/components/Chat.vue'
 
 export default {
-  name: 'App',
-  components: {
-    Header,
-    Footer,
-    Chat
-  },
-  computed: {
-    
-  },
-  methods: {
-    currentPage() {
-      return window.location.pathname;
-    }
-  }, 
-  mounted () {
-    const body = document.querySelector("body")
-    body.style.height = `${window.innerHeight}px` 
+	name: 'App',
+	components: {
+		Header,
+		Footer,
+		Chat
+	},
+	computed: {
+		
+	},
+	methods: {
+		currentPage() {
+			return window.location.pathname;
+		}
+	}, 
+	mounted () {
+		const body = document.querySelector("body")
+		body.style.height = `${window.innerHeight}px` 
 
 
-    const main = document.querySelector("main")
-    main.style.height = `70%` 
-    
-    if(this.currentPage == '/Maps')
-    {
-      // Faire disparaitre les routers
-      document.querySelectorAll("header a").style.display = "none";
-    }
-  }
+		const main = document.querySelector("main")
+		main.style.height = `70%` 
+		
+		if(this.currentPage == '/Maps')
+		{
+			// Faire disparaitre les routers
+			document.querySelectorAll("header a").style.display = "none";
+		}
+	}
 }
 </script>
 
 <style scoped>
-  main{
-        display: flex;
-        flex-direction: row;
-        width: 100%;
-        height: auto;
-    }
+	main{
+				display: flex;
+				flex-direction: row;
+				width: 100%;
+				height: auto;
+		}
 </style>
