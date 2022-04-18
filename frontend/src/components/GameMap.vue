b5403f44ab9958d39d53e6678c868758e91b5d33
BIACHE YAEL <yael.biache@etu.unistra.fr>
2022-04-18 15:24:40 +0200

    get gamemap from frontend

--- frontend/src/components/GameMap.vue
+++ frontend/src/components/GameMap.vue
@@ -1,31 +1,44 @@
 <template>
 	<div>
-		<div>
+		<div id="colonneInfos">
 			<div id="minuteur">
-				<img id="params" alt="Paramètres" title="Paramètres" src="../assets/img/settings.png"/>
+				<img id="params" alt="Paramètres" title="Paramètres"
+					src="../assets/img/settings.png"/>
 				<p>5:30</p>
 			</div>
 			<div id="drapeaux">
 				<h1>Pays</h1>
 				<div>
-					<img alt="Drapeau français" title="France" src="../assets/img/flags/france.png"/>
-					<img alt="Drapeau allemand" title="Allemagne" src="../assets/img/flags/germany.png"/>
-					<img alt="Drapeau italien" title="Italie" src="../assets/img/flags/italy.png"/>
-					<img alt="Drapeau russe" title="Russie" src="../assets/img/flags/russia.png"/>
-					<img alt="Drapeau turque" title="Turquie" src="../assets/img/flags/turkey.png"/>
-					<img alt="Drapeau anglais" title="Angleterre" src="../assets/img/flags/great-britain.png"/>
-					<img alt="Drapeau autrichien" title="Autriche" src="../assets/img/flags/austria-hungary.png"/>
+					<img alt="Drapeau français" title="France"
+						src="../assets/img/flags/france.png"/>
+					<img alt="Drapeau allemand" title="Allemagne"
+						src="../assets/img/flags/germany.png"/>
+					<img alt="Drapeau italien" title="Italie"
+						src="../assets/img/flags/italy.png"/>
+					<img alt="Drapeau russe" title="Russie"
+						src="../assets/img/flags/russia.png"/>
+					<img alt="Drapeau turque" title="Turquie"
+						src="../assets/img/flags/turkey.png"/>
+					<img alt="Drapeau anglais" title="Angleterre"
+						src="../assets/img/flags/great-britain.png"/>
+					<img alt="Drapeau autrichien" title="Autriche"
+						src="../assets/img/flags/austria-hungary.png"/>
 				</div>
 			</div>
 			<div id="chat">
 				<h1>Chat</h1>
+				<div id="historique"></div>
+				<form name="message">
+					<input type="text" name="msg" id="msg"
+						placeholder="Entrez votre message"/>
+				</form>
 			</div>
 		</div>
 		<div id="carte">
 			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 609 559">
 			</svg>
 		</div>
-		<div>
+		<div id="colonneOrdres">
 			<h1>Informations</h1>
 			<div id="ordres">
 				<p>Ordre 1</p>
@@ -40,17 +53,21 @@
 		</div>
 	</div>
 	<dialog id="quitter">
-		<p>Voulez-vous quitter la partie ?</p>
+		<h1>Quitter la partie ?</h1>
 		<form method="dialog">
-			<button value="annuler">Annuler</button>
-			<button value="confirmer">Quitter</button>
+			<div>
+				<button value="annuler">Annuler</button>
+				<button value="confirmer">Quitter</button>
+			</div>
 		</form>
 	</dialog>
 </template>
 
 <script>
-export default {
-	mounted() {
+export default
+{
+	mounted()
+	{
 		let ns = "http://www.w3.org/2000/svg"
 		let svg = document.querySelector("svg")
 		const carte = require("../assets/json/map.json")
@@ -66,32 +83,33 @@ export default {
 				path.setAttribute("fill", "#fcf2d4")
 
 				// Couleur et changement du curseur lors du passage de souris
-				path.addEventListener("mouseover", function ()
+				path.addEventListener("mouseover", function()
 				{
 					this.style.cursor = "pointer"
 					this.style.fill = "lightgreen"
 					this.style.transition = "0.2s"
 				})
-				path.addEventListener("mouseout", function ()
+				path.addEventListener("mouseout", function()
 				{
 					this.style.fill = "#fcf2d4"
 				})
-				path.addEventListener("click", function ()
+				path.addEventListener("click", function()
 				{
-					document.querySelector("#app > div > div:last-child > h1").innerHTML = "Ordres"
+					document.querySelector("#colonneOrdres > h1")
+						.innerHTML = "Ordres"
 					document.querySelector("#infos").style.display = "none"
 					document.querySelector("#ordres").style.display = "flex"
-					console.log(nomZone)
+					console.log("Clic zone terrestre : ", nomZone)
 				})
 			}
 
 			// Zone neutre
 			else if (carte["areas"][j].type == "impassable")
 			{
-				path.setAttribute("fill", "grey")
+				path.setAttribute("fill", "#808080")
 
 				// Changement du curseur
-				path.addEventListener("mouseover", function ()
+				path.addEventListener("mouseover", function()
 				{
 					this.style.cursor = "not-allowed"
 				})
@@ -103,22 +121,23 @@ export default {
 				path.setAttribute("fill", "#b4b6cc")
 
 				// Couleur et changement du curseur lors du passage de souris
-				path.addEventListener("mouseover", function ()
+				path.addEventListener("mouseover", function()
 				{
 					this.style.cursor = "pointer"
 					this.style.fill = "lightblue"
 					this.style.transition = "0.2s"
 				})
-				path.addEventListener("mouseout", function ()
+				path.addEventListener("mouseout", function()
 				{
 					this.style.fill = "#b4b6cc"
 				})
-				path.addEventListener("click", function ()
+				path.addEventListener("click", function()
 				{
-					document.querySelector("#app > div > div:last-child > h1").innerHTML = "Ordres"
+					document.querySelector("#colonneOrdres > h1")
+						.innerHTML = "Ordres"
 					document.querySelector("#infos").style.display = "none"
 					document.querySelector("#ordres").style.display = "flex"
-					console.log(nomZone)
+					console.log("Clic zone maritime : ", nomZone)
 				})
 			}
 
@@ -129,6 +148,8 @@ export default {
 
 		for (var k in carte["infos"])
 		{
+			let pays = k // obligatoire, sinon toujours "Yor"
+
 			// Labels
 			let point = document.createElementNS(ns, "text")
 			var text = document.createTextNode(k)
@@ -137,7 +158,7 @@ export default {
 			point.appendChild(text)
 
 			// Empêche la selection du label
-			point.addEventListener("mouseover", function ()
+			point.addEventListener("mouseover", function()
 			{
 				this.style.pointerEvents = "none"
 			})
@@ -163,38 +184,170 @@ export default {
 				circleOut.setAttribute("stroke", "black")
 
 				// Couleur et changement du curseur lors du passage de souris
-				circleOut.addEventListener("mouseover", function ()
+				circleOut.addEventListener("mouseover", function()
 				{
 					this.style.cursor = "pointer"
 					this.style.fill = "lightcoral"
 				})
-				circleOut.addEventListener("mouseout", function ()
+				circleOut.addEventListener("mouseout", function()
 				{
 					this.style.fill = "none"
 				})
-				circleOut.addEventListener("click", function ()
+				circleOut.addEventListener("click", function()
 				{
-					alert("Clic ravitaillement")
+					console.log("Clic ravitaillement : ", pays)
 				})
 
 				svg.appendChild(circleIn)
 				svg.appendChild(circleOut)
 			}
+
+			// Pion marqueur
+			if (k == "Par")
+			{
+				let marqueur = document.createElementNS(ns, "circle")
+
+				marqueur.setAttribute("cx", carte["infos"][k].coords[0]-5)
+				marqueur.setAttribute("cy", carte["infos"][k].coords[1])
+				marqueur.setAttribute("r", 2.5)
+				marqueur.setAttribute("fill", "red")
+				marqueur.setAttribute("stroke", "red")
+				svg.appendChild(marqueur)
+
+				// Couleur et changement du curseur lors du passage de souris
+				marqueur.addEventListener("mouseover", function()
+				{
+					this.style.cursor = "pointer"
+					this.style.fill = "white"
+				})
+				marqueur.addEventListener("mouseout", function()
+				{
+					this.style.fill = "red"
+				})
+				marqueur.addEventListener("click", function()
+				{
+					console.log("Clic marqueur : ", pays)
+				})
+			}
+
+			// Pion flotte
+			if (k == "Nwg")
+			{
+				let flotte = document.createElementNS(ns, "ellipse")
+
+				flotte.setAttribute("cx", carte["infos"][k].coords[0]-7.5)
+				flotte.setAttribute("cy", carte["infos"][k].coords[1])
+				flotte.setAttribute("rx", 5)
+				flotte.setAttribute("ry", 2)
+				flotte.setAttribute("fill", "blue")
+				flotte.setAttribute("stroke", "blue")
+				svg.appendChild(flotte)
+
+				// Couleur et changement du curseur lors du passage de souris
+				flotte.addEventListener("mouseover", function()
+				{
+					this.style.cursor = "pointer"
+					this.style.fill = "white"
+				})
+				flotte.addEventListener("mouseout", function()
+				{
+					this.style.fill = "blue"
+				})
+				flotte.addEventListener("click", function()
+				{
+					console.log("Clic flotte : ", pays)
+				})
+			}
+
+			// Pion armée
+			if (k == "Gal")
+			{
+				let armee = document.createElementNS(ns, "rect")
+
+				armee.setAttribute("x", carte["infos"][k].coords[0]-7.5)
+				armee.setAttribute("y", carte["infos"][k].coords[1])
+				armee.setAttribute("width", 5)
+				armee.setAttribute("height", 5)
+				armee.setAttribute("fill", "green")
+				armee.setAttribute("stroke", "green")
+				svg.appendChild(armee)
+
+				// Couleur et changement du curseur lors du passage de souris
+				armee.addEventListener("mouseover", function()
+				{
+					this.style.cursor = "pointer"
+					this.style.fill = "white"
+				})
+				armee.addEventListener("mouseout", function()
+				{
+					this.style.fill = "green"
+				})
+				armee.addEventListener("click", function()
+				{
+					console.log("Clic armee : ", pays)
+				})
+			}
 		}
 
 		// Pour quitter la partie
-		let paramBtn = document.getElementById("params");
-		let quitDialog = document.getElementById("quitter");
+		let paramBtn = document.getElementById("params")
+		let quitDialog = document.getElementById("quitter")
 
-		paramBtn.addEventListener("click", function onOpen() {
-			if (typeof quitDialog.showModal === "function") {
+		paramBtn.addEventListener("click", function onOpen()
+		{
+			if (typeof quitDialog.showModal === "function")
 				quitDialog.showModal();
-			}
 		})
 
 		// Action effectuée lors de l'appuie sur l'un des boutons
-		quitDialog.addEventListener("close", function onClose() {
+		quitDialog.addEventListener("close", function onClose()
+		{
 			console.log(quitDialog.returnValue)
+		})
+
+		// Action effectuée quand on appuie sur "Entrer" dans le chat
+		let texte = document.querySelector("input[type=text]")
+		document.querySelector("form").onkeypress = function(e)
+		{
+			if (e.key === "Enter")
+			{
+				e.preventDefault()
+				if (texte.value != "")
+				{
+					// Récupérer le pseudo là dedans
+					var pseudo = "Patrick"
+
+					// Créer le message
+					var para = document.createElement("p")
+					var contenu = document.createTextNode(pseudo + " : " +
+						texte.value)
+					para.appendChild(contenu)
+					para.style.textAlign = "left"
+					para.style.margin = "0"
+
+					// Changer la couleur du joueur en fonction du pays
+					para.style.color = "wheat"
+
+					let historique = document.getElementById("historique")
+					historique.appendChild(para)
+
+					// Réinitialiser l'input et afficher le dernier message
+					texte.value = ""
+					historique.scrollTop = historique.scrollHeight
+				}
+			}
+		}
+
+		// Affiche ou masque l'historique et l'input
+		var $ = require("jquery")
+		document.querySelector("#chat > h1").addEventListener("click", () =>
+		{
+			if (window.innerWidth < 769)
+			{
+				$(document.getElementById("historique")).slideToggle(100)
+				$(document.getElementsByTagName("form")[0]).slideToggle(100)
+				$(document.querySelector("#chat > h1")).toggleClass("bas")
+			}
 		})
 	}
 }
@@ -209,14 +362,10 @@ export default {
 	img{
 		cursor: pointer;
 	}
-	h1, p{
-		text-align: center;
-	}
 
 	/* Carte */
 	#carte{
 		border-radius: 10px;
-		box-shadow: 0px 0px 15px 5px #CECECE;
 		width: fit-content;
 		height: 98vh;
 		margin: 1vh 0 1vh 0;
@@ -227,21 +376,19 @@ export default {
 		cursor: not-allowed;
 	}
 	svg{
-		background-color: #535353;
+		background-color: rgba(112, 128, 143, 0.9);
 		height: 100%;
-
-		/* Style pour le blocage de la carte */
-		/* pointer-events: none;
-		filter: grayscale(1) invert(0.1); */
+	}
+	.bloque{
+		pointer-events: none;
+		filter: grayscale(1) invert(0.1);
 	}
 
-	/* Colonne de gauche */
-	#app > div > div:first-child,
-	#app > div > div:last-child{
+	/* Colonnes */
+	#colonneInfos,
+	#colonneOrdres{
 		width: 25vw;
 		height: 98vh;
-		background-color: white;
-		box-shadow: 0px 0px 15px 5px #CECECE;
 		border-radius: 10px;
 		margin: 1vh 1vw 1vh 1vw;
 	}
@@ -250,7 +397,7 @@ export default {
 	#minuteur{
 		display: flex;
 		justify-content: space-between;
-		border-bottom: grey 3px solid;
+		height: 88px;
 	}
 	#minuteur > img{
 		width: 48px;
@@ -274,7 +421,8 @@ export default {
 	#drapeaux{
 		display: flex;
 		flex-direction: column;
-		height: 30%;
+		height: 35%;
+		overflow-x: auto;
 	}
 	#drapeaux > h1{
 		font-size: 35px;
@@ -293,23 +441,27 @@ export default {
 		user-select: none;
 	}
 
-	/* Chat (masqué pendant l'alpha) */
+	/* Chat */
 	#chat{
-		display: none; /* Placeholder pour plus tard */
-		width: 30vw;
-		height: calc(60% - 48px);
-		border-radius: 10px;
+		width: 100%;
+		height: calc(65% - 92px);
+		background-color: unset;
+	}
+	#chat > h1{
+		padding-top: 20px;
+		border-style: solid;
+		border-width: 4px 0 0;
+		border-image: radial-gradient(#ae0132, #1c0043) 1;
 	}
 
 	/* Colonne d'ordres */
-	#app > div > div:last-child{
+	#colonneOrdres{
 		width: 18vw;
 		height: 98vh;
 	}
-	#app > div > div:last-child > h1{
+	#colonneOrdres > h1{
 		line-height: 88px;
 		margin: 0;
-		border-bottom: grey 3px solid;
 	}
 	#infos,
 	#ordres{
@@ -322,35 +474,18 @@ export default {
 	}
 	#infos > p{
 		margin: 0;
-		font-size: 25px;
 	}
 	#ordres{
 		display: none;
 	}
-	#ordres > p,
-	#quitter > form > button{
-		background-color: #F0F0F0;
-		padding: 0 10% 0 10%;
+	#ordres > p{
 		margin: 0 10px;
+		padding: 0 10% 0 10%;
 		line-height: 55px;
-		font-size: 25px;
-		border-radius: 10px;
-		border-style: grey 3px solid;
-		cursor: pointer;
-		transition: 0.3s;
 		font-weight: bold;
 	}
-	#ordres > p:hover,
-	#quitter > form > button:hover{
-		background-color: #e4e4e4;
-	}
-	#ordres > p:active,
-	#quitter > form > button:active{
-		background-color: #d6d6d6;
-	}
 	#ordres > p:last-child{
-		background-color: grey;
-		color: white;
+		background-color: #808080;
 		padding: 0 20% 0 20%;
 	}
 	#ordres > p:last-child:hover{
@@ -362,38 +497,32 @@ export default {
 
 	/* Boîte de dialogue pour quitter */
 	#quitter{
-		background-color: white;
-		border-radius: 10px;
-		border-style: none;
-		box-shadow: 0px 0px 15px 5px #5b5b5b;
-	}
-	#quitter > p{
-		font-size: 25px;
-		padding: 0 30px;
-	}
-	#quitter > form{
-		display: flex;
-		justify-content: space-evenly;
-	}
-	#quitter > form > button{
-		border: none;
-		line-height: 40px;
-		margin: 0 10px;
-		outline: inherit;
-		flex-basis: 50%;
+		min-width: 25vw;
 	}
 
+/* Mode sombre */
+@media(prefers-color-scheme: dark){
+	/* Carte */
+	svg{
+		background-color: rgba(42, 58, 73, 0.9);
+	}
+}
+
 /* Version tablette */
-@media screen and (min-width:770px) and (max-width:1370px){
+@media screen and (min-width: 770px) and (max-width: 1370px){
+	/* Div principale */
+	#app > div{
+		flex-wrap: wrap;
+	}
 	/* Carte */
 	#carte{
+		width: calc(75vw - 3vw);
 		margin: 1vh 1vw 1vh 0;
 		font-size: 10px;
 	}
 
-	/* Colonne de gauche */
-	#app > div > div:first-child,
-	#app > div > div:last-child{
+	/* Colonnes */
+	#colonneInfos{
 		height: 98vh;
 	}
 
@@ -416,17 +545,21 @@ export default {
 	}
 
 	/* Colonne d'ordres */
-	#infos > p{
-		font-size: 32px;
+	#colonneOrdres{
+		width: 100%;
+		height: 35vh;
+	}
+	#ordres{
+		flex-direction: row;
 	}
 	#ordres > p,
 	#quitter > form > button{
-		font-size: 22px;
+		width: 25%;
 	}
 }
 
 /* Version mobile */
-@media screen and (max-width:769px){
+@media screen and (max-width: 769px){
 	/* Div principale */
 	#app > div{
 		flex-direction: column;
@@ -439,8 +572,9 @@ export default {
 		font-size: 11px;
 	}
 
-	/* Colonne de gauche */
-	#app > div > div:first-child{
+	/* Colonnes */
+	#colonneInfos,
+	#colonneOrdres{
 		width: 96vw;
 		height: max-content;
 		margin: 1vh 2vw 1vh 2vw;
@@ -464,13 +598,11 @@ export default {
 	}
 
 	/* Colonne d'ordres */
+	#colonneOrdres{
+		margin: 1vh 2vw 1vh 2vw;
+	}
 	#ordres{
 		flex-direction: row;
-	}
-	#app > div > div:last-child{
-		width: 96vw;
-		margin: 1vh 2vw 1vh 2vw;
-		height: max-content;
 	}
 	#infos > p{
 		margin: 30px 0;
@@ -480,40 +612,6 @@ export default {
 	#quitter > form > button{
 		font-size: 22px;
 		margin: 10px;
-	}
-}
-
-/* Thème sombre */
-@media (prefers-color-scheme: dark){
-	/* Colonnes */
-	#app > div > div:first-child,
-	#app > div > div:last-child,
-	#carte,
-	#quitter{
-		background-color: #232224;
-		box-shadow: 0px 0px 15px 5px #131313;
-	}
-	#minuteur > img{
-		filter: grayscale(1) invert(1);
-	}
-
-	/* Boutons */
-	#ordres > p,
-	#quitter > form > button{
-		background-color: #bbbbbb;
-	}
-	#ordres > p:hover,
-	#quitter > form > button:hover{
-		background-color: #a0a0a0;
-	}
-	#ordres > p:active,
-	#quitter > form > button:active{
-		background-color: #868686;
-	}
-
-	/* Boîte de dialogue pour quitter */
-	#quitter{
-		box-shadow: 0px 0px 15px 5px #424242;
 	}
 }
 </style>
