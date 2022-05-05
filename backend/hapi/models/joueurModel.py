9afac42f85b1f707ab9bd115a942e4f4a3a4674d
Loïc DIEBOLT <loic@dblt.fr>
2022-05-05 23:52:23 +0200

    Fix git merge

---
+++ backend/hapi/models/joueurModel.py
@@ -0,0 +1,24 @@
+from email import message
+from sqlalchemy import *
+from sqlalchemy.orm import relationship
+
+from hapi.models import Base
+
+class Joueur(Base):
+    __tablename__="joueur"
+    idJoueur = Column(Integer, primary_key=True)
+    pseudoJoueur=Column(String(255), unique=True, nullable=False)
+    idPartie=Column(Integer ,ForeignKey('partie.idPartie'))
+    state=Column(String(255)) # s'il est connecté ou pas 
+    cookies=Column(String(255), unique=True)
+    message=relationship('ChatModel')
+    
+    region=relationship('RegionDunJoueur')
+    
+    puissance_c=relationship('PuissanceModel',
+                          secondary='puissanceJoueur',
+                          back_populates='joueur')
+    ordre=relationship('OrdreModel',
+                          secondary='ordreJoueur',
+                          back_populates='joueur')
+    
